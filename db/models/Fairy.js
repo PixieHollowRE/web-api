bc105b316413e17c7bbea2599b6e2810ace95e32
Jaden <me@rocketprogrammer.me>
2026-06-18 02:16:26 -0500

    these

--- db/models/Fairy.js
+++ db/models/Fairy.js
@@ -38,6 +38,8 @@ const Fairy = new mongoose.model('Fairy', {
   level: { type: Number, default: 0 },
   dailyChanceLastSpinDay: { type: Number, default: 0 },
   lastAckMemberDays: { type: Number, default: -1 },
+  banned: { type: Number, default: 0 },
+  terminated: { type: Number, default: 0 },
   avatar: {
     proportions: {
       head: Number,
